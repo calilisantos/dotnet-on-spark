1ed04213d48bacb372415548a7b3190ec231825c
calilisantos <calilisantos@gmail.com>
2023-08-27 17:07:15 -0300

    feat(Code): up files

---
+++ Code/Program.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Spark.Sql;
+using static Microsoft.Spark.Sql.Functions;
+
+namespace MySparkApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create Spark session
+            SparkSession spark =
+                SparkSession
+                    .Builder()
+                    .AppName("flow_of_cash")
+                    .GetOrCreate();
+
+            // Create initial DataFrame
+            string filePath = args[0];
+            DataFrame dataFrame = spark.Read().Parquet(filePath);
+
+            // Display initial DataFrame
+            dataFrame.Show();
+            //Create accurate DataFrame
+            DataFrame acDataframe =
+                dataFrame
+                    .Select("date", "cash_flow", "accumulated_cash")
+                    .WithColumn("date", DateFormat(Col("date"), "dd/MM/yyy"));
+
+            // Display results
+            acDataframe.Show();
+
+            // Show the last value of accumulated cash
+            acDataframe
+                .OrderBy(Desc("date"))
+                .Limit(1)
+                .Show();
+
+            // Stop Spark session
+            spark.Stop();
+        }
+    }
+}
